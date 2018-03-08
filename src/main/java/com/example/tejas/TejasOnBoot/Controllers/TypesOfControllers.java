10d47066dbdcd7907c1c0ed1488752682dcff9d0
tejas1996 <tejas.dond@ves.ac.in>
2018-03-08 23:56:21 +0530

    Adding few controllers and methods to get and add data and articles ro a simple hashmap for now

--- src/main/java/com/example/tejas/TejasOnBoot/Controllers/TypesOfControllers.java
+++ src/main/java/com/example/tejas/TejasOnBoot/Controllers/TypesOfControllers.java
@@ -1,5 +1,6 @@
 package com.example.tejas.TejasOnBoot.Controllers;
 
+import com.example.tejas.TejasOnBoot.Data;
 import com.example.tejas.TejasOnBoot.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,19 +15,19 @@ public class TypesOfControllers {
     @Autowired
     StudentService studentService;
 
+    @Autowired
+    Data data;
+
     // just a normal controller to handle get requests
     @GetMapping("/home")
     public String greetings(Map<String, Object> model) {
-        model.put("message", "Hello Sev");
+
+        model.put("message", data.getAll());
         return "Hello";
     }
 
     // Controllers with a path variable
     // In this type we use a {} to define a variable and use the anotation called path variable
-    @RequestMapping("/home/{id}")
-    public String singleStudent(@PathVariable String id) {
-        return studentService.getSingle(id);
-    }
 
 
 }
